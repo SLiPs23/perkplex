b69e6378a6c6af38e337c0eed3636de1b7c4a5ed
Sean Perkins <sperkins@Perk-Kube-Master.perkplex.geekgalaxy.com>
2020-03-12 16:42:46 -0400

    lots of changes

--- 2-Traefik/external/deploy_consul_cluster.sh
+++ 2-Traefik/external/deploy_consul_cluster.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 # Version 1.1.0 is needed because the consul manifest doesn't have the 1.0.0 image for ARM
-helm install --name consul-traefik stable/consul --set ImageTag=1.4.3
-
-cat << EOF
-apiVersion: extensions/v1beta1
-kind: Ingress
-metadata:
-  name: consul-ui
-  namespace: kube-system
-spec:
-  rules:
-  - host: consul.int.perkplex.geekgalaxy.com
-    http:
-      paths:
-      - path: /
-        backend:
-          serviceName: consul-traefik-ui
-          servicePort: http
-EOF | kubectl create -f -
+helm install consul-traefik stable/consul
