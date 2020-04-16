fe9382cd4e60e50c285919020a662519e2f71d3a
Sean Perkins <seanlperkins@icloud.com>
2020-04-16 13:31:09 -0400

    lots of changes

--- metallb/install_metallb.sh
+++ metallb/install_metallb.sh
@@ -1,3 +1,3 @@
-kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.3/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/metallb.yaml
 kubectl apply -f metallb-conf.yaml
 
